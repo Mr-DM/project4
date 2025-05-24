6b619c892b182a7e88c83263ac0b3436869af1a1
Mr-Dm <danm306009@gmail.com>
2025-05-24 16:31:32 +0300

    First commit

---
+++ src/pack/CellularPlane.java
@@ -0,0 +1,92 @@
+/* CellularPlane
+ * Description:
+ * version: v1.0
+ * Author: Dan Morozov
+*/
+
+package pack;
+import java.util.*;
+
+public class CellularPlane {
+    private String _namePack;
+    private double _price;
+    private int _numFmin;//Number of browsing and Num of MB
+    private int _numFMB;
+    private double _overFmin;//Over of minutes
+    private double _overFmb;//Over of MB
+
+
+    private double _priceFminOver;
+    private double _priceFmbOver;
+
+    public CellularPlane(String namePack)
+    {
+        _namePack = namePack;
+    }
+
+    public String PriceperMonth(int numFmin, int numFMB , int UsedDays) {
+        int UsedMIN = numFmin;
+        int UsedMB = numFMB;
+        if(_namePack.equals("Basic")){
+            _numFMB = 2000;
+            _numFmin = 200;
+            _price = 19.90;
+        }
+        else if(_namePack.equals("Premium")){
+            _numFMB = 5000;
+            _numFmin = 500;
+            _price = 29.90;
+        }
+        else if(_namePack.equals("Unlimited")){
+            _numFMB = 10000;
+            _numFmin = 1000;
+            _price = 49.90;
+        }
+        else {
+            return "Error: Invalid package name.";
+        }
+        double DaysFMounth = 28.0 / UsedDays;
+        double PricePerDay = _price * DaysFMounth;
+
+        double Tempmin = _numFmin * DaysFMounth;// number of minutes needed be used
+        double TempMB = _numFMB * DaysFMounth; // number of MB needed be used
+
+        if(UsedMIN > Tempmin){
+            _overFmin = UsedMIN - Tempmin;
+            _priceFminOver = _overFmin * 0.3;
+        } else {
+            _overFmin = 0;
+            _priceFminOver = 0;
+        }
+
+        if(UsedMB > TempMB){
+            _overFmb = UsedMB - TempMB;
+            _priceFmbOver = _overFmb * 0.3;
+        } else {
+            _overFmb = 0;
+            _priceFmbOver = 0;
+        }
+        _price = PricePerDay + _priceFminOver + _priceFmbOver;
+
+        return "The price for the month is: "+ _price + "$";
+    }
+
+    public String toString(){
+        return "Price:"+_price+"$" +"\n"+
+        "Number of minutes:"+_numFmin+" min" +"\n"+
+        "Number of MB:"+_numFMB+" MB"+'\n'+
+        "Price per minute of call beyound the limit:"+_priceFminOver+"$" +'\n'+
+        "Price per MB beyound the limit:"+_priceFmbOver+"$";
+
+    }
+
+    public double getPrice() {
+        return _price;
+    }
+    public int getNumFmin() {
+        return _numFmin;
+    }
+    public int getNumFMB() {
+        return _numFMB;
+    }
+}
