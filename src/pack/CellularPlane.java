ce7a7f8d7c2c205512bd544c3044dec89310fd50
Mr-Dm <danm306009@gmail.com>
2025-06-14 14:53:34 +0300

    Chenge all the work

--- src/pack/CellularPlane.java
+++ src/pack/CellularPlane.java
@@ -6,124 +6,71 @@
 */
 
 package pack;
+import java.lang.classfile.MethodBuilder;
 import java.util.*;
 
 public class CellularPlane {
-    private String _namePack;
-    private double _price;
-    private int _numFmin;//Number of browsing and Num of MB
-    private int _numFMB;
-    private double _overFmin;//Over of minutes
-    private double _overFmb;//Over of MB
+    private int _NamePack;
+    private int _Min;
+    private int _MB;
+    private float _Price;
+    private int _OverMin;
+    private int _OverMB;
 
 
-    private double _priceFminOver;
-    private double _priceFmbOver;
 
-    // the constructor of the CellularPlane class
-    public CellularPlane(String namePack)
-    {   
-        _namePack = namePack;
-        Checker(namePack);
-       }
-    
-    public void Checker(String namePack) {
-        if(namePack.toLowerCase().equals("basic")){
-            _numFMB = 2000;
-            _numFmin = 200;
-            _price = 19.90;
-        }
-        else if(namePack.toLowerCase().equals("premium")){
-            _numFMB = 5000;
-            _numFmin = 500;
-            _price = 29.90;
-        }
-        else if(namePack.toLowerCase().equals("unlimited")){
-            _numFMB = 10000;
-            _numFmin = 1000;
-            _price = 49.90;
-        }else{
-            _numFMB = 2000;
-            _numFmin = 200;
-            _price = 19.90;
-
-        }
+    public String toString() {
+        return "CellularPlane{" +
+                "_NamePack=" + _NamePack +
+                ", _Min=" + _Min +
+                ", _MB=" + _MB +
+                ", _Price=" + _Price +
+                '}';
     }
 
-    public String PriceperMonth(int UsedMIN, int UsedMB){
-        
-        if(UsedMIN > _numFmin){
-            _overFmin = UsedMIN - _numFmin;
-            _priceFminOver = _overFmin * 0.3;
-        } else {
-            _overFmin = 0;
-            _priceFminOver = 0;
-        }
 
-        if(UsedMB > _numFMB){
-            _overFmb = UsedMB - _numFMB;
-            _priceFmbOver = _overFmb * 0.05;
-        } else {
-            _overFmb = 0;
-            _priceFmbOver = 0;
-        }
-        _price = _priceFminOver + _priceFmbOver + _price;
-
-        return "The price for the month is: "+ _price + "$";
-    }
-    public String PriceperMonth(int numFmin, int numFMB , int UsedDays) {
-        int UsedMIN = numFmin;
-        int UsedMB = numFMB;
-
-        double DaysFMounth = 28.0 / UsedDays;
-        double PricePerDay = _price * DaysFMounth;
-
-        double Tempmin = _numFmin * DaysFMounth;// number of minutes needed be used
-        double TempMB = _numFMB * DaysFMounth; // number of MB needed be used
-
-        if(UsedMIN > Tempmin){
-            _overFmin = UsedMIN - Tempmin;
-            _priceFminOver = _overFmin * 0.3;
-        } else {
-            _overFmin = 0;
-            _priceFminOver = 0;
-        }
-
-        if(UsedMB > TempMB){
-            _overFmb = UsedMB - TempMB;
-            _priceFmbOver = _overFmb * 0.05;
-        } else {
-            _overFmb = 0;
-            _priceFmbOver = 0;
-        }
-        _price = PricePerDay + _priceFminOver + _priceFmbOver;
-
-        return "The price for the month is: "+ _price + "$";
+    public CellularPlane(int NamePack, int Min, int MB, float Price){
+        _NamePack = NamePack;
+        _Min = Min <= 0 ? 0 : Min;
+        _MB = MB <= 0 ? 0 : MB;
+        _Price = Price <= 0 ? 0 : Price;
     }
 
-    public String toString(){
-        return "Price:"+_price+"$" +"\n"+
-        "Number of minutes:"+_numFmin+" min" +"\n"+
-        "Number of MB:"+_numFMB+" MB"+'\n'+
-        "Price per minute of call beyound the limit:"+_priceFminOver+"$" +'\n'+
-        "Price per MB beyound the limit:"+_priceFmbOver+"$";
+    public String PriceForMonth(int UsedMin, int UsedMB) {
+        _OverMin = UsedMB > _Min ? 0 : UsedMin - _Min;
+        _OverMB = UsedMB > _MB ? 0 : UsedMB - _MB;
+        double PriceForMonth = _Price + (_OverMin * 0.3) + (_OverMB * 0.05);
+        return "Price for the month: " + PriceForMonth;
+    }
 
+    public String PriceForMonth(int UsedMB, int UsedMin, int UsedDay) {
+        float NeedbeUsed = 28 / UsedDay;
+        float priceFUsedDay = NeedbeUsed * _Price;
+        _OverMin = UsedMB > _Min ? 0 : UsedMin - _Min;
+        _OverMB = UsedMB > _MB ? 0 : UsedMB - _MB;
+        double PriceForMonth = priceFUsedDay + (_OverMin * 0.3) + (_OverMB * 0.05);
+        return "Price for the month: " + PriceForMonth;
     }
-    public String getNamePack() {
-        return _namePack;
+    public void Same(CellularPlane other) {
+        if(_MB == other.getMB())
+        {System.out.println("The MB are the same");}
+        if(_Min == other.getMin())
+        {System.out.println("The Min are the same");}
+        if(_Price == other.getPrice())
+        {System.out.println("The Price are the same");}
     }
-    public double getPrice() {
-        return _price;
+
+
+    public float getPrice(){
+        return _Price;
     }
-    public int getNumFmin() {
-        return _numFmin;
+    public int getMin(){
+        return _Min;
     }
-    public int getNumFMB() {
-        return _numFMB;
+    public int getMB(){
+        return _MB;
     }
-    
-    public void setNamePack(String namePack) {
-        _namePack = namePack;
-        Checker(namePack);
+    public int getNamePack(){
+        return _NamePack;
     }
 }
